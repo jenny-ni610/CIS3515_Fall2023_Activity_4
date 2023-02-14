92e5262bcda985c33a2982ba91b1f86c2d5ba04b
Jenny Ni <tun48961@temple.edu>
2023-02-14 12:16:30 -0500

    created TextSizeAdapter class

--- app/src/main/java/edu/temple/activity4/MainActivity.kt
+++ app/src/main/java/edu/temple/activity4/MainActivity.kt
@@ -2,8 +2,12 @@ package edu.temple.activity4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,14 +18,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        textSizeSelector = findViewById(R.id.textSizeSelectorRecyclerView)
+        textSizeDisplay = findViewById(R.id.textSizeDisplayTextView)
         // Trying to create array of integers that are multiples of 5
         // Verify correctness by examining array values.
         val textSizes = Array(20){(it + 1) * 5}
+
+        //for(i in 0 <= until < textSizes.size) {
+        //    Log.d("Array values", textSizes[1].toString())
+        //}
+        textSizeSelector.adapter = TextSizeAdapter(textSizes);
+        textSizeSelector.layoutManager = LinearLayoutManager(this)
     }
 }
 
-
 /* Convert to RecyclerView.Adapter */
-class TextSizeAdapter {
+class TextSizeAdapter(_textSizes: Array<Int>): RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>(){
+    private val textSizes = _textSizes
+    class TextSizeViewHolder(view: TextView): RecyclerView.ViewHolder(view) {
+        val textView = view
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextSizeViewHolder {
+        return TextSizeViewHolder(TextView(parent.context).apply {
+            setPadding(5, 20, 0, 20)
+        })
+    }
 
+    override fun getItemCount(): Int {
+        return textSizes.size
+    }
+
+    override fun onBindViewHolder(holder: TextSizeViewHolder, position: Int) {
+        holder.textView.text = textSizes[position].toString()
+        holder.textView.textSize = textSizes[position].toFloat()
+    }
 }
